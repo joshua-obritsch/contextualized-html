b07588a938d9c8b9bef302eb084762185da76d80
Joshua Obritsch <joshua@obritsch.com>
2024-06-30 11:05:51 +0200

    Refactor

--- src/Html/Internal.elm
+++ src/Html/Internal.elm
@@ -1,11 +1,10 @@
 module Html.Internal exposing
     ( Html(..), Attribute(..)
-    , decontextualizeHtml, decontextualizeAttribute
-    , contextualizeHtml, contextualizeAttribute
+    , contextualizeHtml, contextualizeAttribute, decontextualizeHtml, decontextualizeAttribute
     , usingHtml, usingAttribute
     )
 
-{-| This module provides a set of primitives for generating context-dependent HTML elements and attributes.
+{-| This module provides a set of internal types and functions.
 
 
 # Primitives
@@ -13,17 +12,12 @@ module Html.Internal exposing
 @docs Html, Attribute
 
 
-# Decontextualization
+# Contextual Conversion
 
-@docs decontextualizeHtml, decontextualizeAttribute
-
-
-# Contextualization
-
-@docs contextualizeHtml, contextualizeAttribute
+@docs contextualizeHtml, contextualizeAttribute, decontextualizeHtml, decontextualizeAttribute
 
 
-# Context Dependency
+# Contextual Dependency
 
 @docs usingHtml, usingAttribute
 
@@ -36,67 +30,55 @@ import VirtualDom
 -- PRIMITIVES
 
 
-{-| Represents an HTML element encompassing a context (`ctx`) and message (`msg`), also known as a contextualized HTML element.
--}
 type Html ctx msg
     = Html (ctx -> VirtualDom.Node msg)
 
 
-{-| Represents an HTML attribute encompassing a context (`ctx`) and message (`msg`), also known as a contextualized HTML attribute.
--}
 type Attribute ctx msg
     = Attribute (ctx -> VirtualDom.Attribute msg)
 
 
 
--- DECONTEXTUALIZATION
+-- CONTEXTUAL CONVERSION
 
 
-{-| Converts a contextualized HTML element into a decontextualized virtual DOM node.
--}
+contextualizeHtml : VirtualDom.Node msg -> Html ctx msg
+contextualizeHtml html =
+    Html <|
+        \_ -> html
+
+
+contextualizeAttribute : VirtualDom.Attribute msg -> Attribute ctx msg
+contextualizeAttribute attribute =
+    Attribute <|
+        \_ -> attribute
+
+
 decontextualizeHtml : ctx -> Html ctx msg -> VirtualDom.Node msg
 decontextualizeHtml ctx (Html decontextualize) =
     decontextualize ctx
 
 
-{-| Converts a contextualized HTML attribute into a decontextualized virtual DOM attribute.
--}
 decontextualizeAttribute : ctx -> Attribute ctx msg -> VirtualDom.Attribute msg
 decontextualizeAttribute ctx (Attribute decontextualize) =
     decontextualize ctx
 
 
 
--- CONTEXTUALIZATION
+-- CONTEXTUAL DEPENDENCY
 
 
-{-| Converts a decontextualized virtual DOM attribute into a contextualized HTML attribute.
--}
-contextualizeAttribute : VirtualDom.Attribute msg -> Attribute ctx msg
-contextualizeAttribute content =
-    Attribute (\_ -> content)
-
-
-{-| Converts a decontextualized virtual DOM node into a contextualized HTML element.
--}
-contextualizeHtml : VirtualDom.Node msg -> Html ctx msg
-contextualizeHtml content =
-    Html (\_ -> content)
-
-
-
--- CONTEXT DEPENDENCY
-
-
-{-| Establishes a dependency between a context and an HTML element.
--}
 usingHtml : (ctx -> Html ctx msg) -> Html ctx msg
 usingHtml decontextualize =
-    Html (\ctx -> decontextualizeHtml ctx <| decontextualize ctx)
+    Html <|
+        \ctx ->
+            decontextualizeHtml ctx <|
+                decontextualize ctx
 
 
-{-| Establishes a dependency between a context and an HTML attribute.
--}
 usingAttribute : (ctx -> Attribute ctx msg) -> Attribute ctx msg
 usingAttribute decontextualize =
-    Attribute (\ctx -> decontextualizeAttribute ctx <| decontextualize ctx)
+    Attribute <|
+        \ctx ->
+            decontextualizeAttribute ctx <|
+                decontextualize ctx
